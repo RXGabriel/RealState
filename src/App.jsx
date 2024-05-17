dacfddebb3e88c52f921d027645094b2ca4e1ea9
RXGabriel <gabriel2005rf@gmail.com>
2024-05-17 11:21:25 -0300

    feat: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header/header";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
