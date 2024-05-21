dba70e9f6734574e5e1d90e1c0fc40967d9f8150
RXGabriel <gabriel2005rf@gmail.com>
2024-05-21 10:23:43 -0300

    feat: add left side of Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        <div className="flexColStart f-left">
+          <img src="./logo2.png" alt="logo2" width={120} />
+          <span className="secondaryText">
+            Our vision is to make all people
+            <br />
+            the best place to live for them.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
