c724d65b6deff3c4f3a3e639e2b2041456fba5c9
RXGabriel <gabriel2005rf@gmail.com>
2024-05-29 10:52:47 -0300

    fix: correct Header import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/header";
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
 import Companies from "./components/Companies/Companies";
