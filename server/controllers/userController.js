78461df2f35c759e426992b727a13deb9824de8f
RXGabriel <gabriel2005rf@gmail.com>
2024-05-31 15:10:57 -0300

    feat: add bookVisit functionality in userController and userRoute

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,3 +11,31 @@ export const createUser = asyncHandler(async (req, res) => {
     res.send({ message: "User created successfully", user: user });
   } else res.status(201).send({ message: "User already exists" });
 });
+
+export const bookVisit = asyncHandler(async (req, res) => {
+  const { email, date } = req.body;
+  const { id } = req.params;
+
+  try {
+    const alreadyBooked = await prisma.user.findUnique({
+      where: { email },
+      select: { bookedVisits: true },
+    });
+
+    if (alreadyBooked.bookedVisits.some((visit) => visit.id === id)) {
+      res
+        .status(400)
+        .json({ message: "This residency is already booked by you" });
+    } else {
+      await prisma.user.update({
+        where: { email: email },
+        data: {
+          bookedVisits: { push: { id, date } },
+        },
+      });
+      res.send("your visit is booked successfully");
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
