3770146906c9b2d1d9ab02bf339c6bd5a2321db5
RXGabriel <gabriel2005rf@gmail.com>
2024-05-31 15:47:05 -0300

    feat: add cancelBooking functionality in userController and userRoute

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -53,3 +53,30 @@ export const allBookings = asyncHandler(async (req, res) => {
     throw new Error(error.message);
   }
 });
+
+export const cancelBooking = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const { id } = req.params;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      select: { bookedVisits: true },
+    });
+
+    const index = user.bookedVisits.findIndex((visit) => visit.id === id);
+
+    if (index === -1) {
+      res.status(400).json({ message: "Booking not found" });
+    } else {
+      user.bookedVisits.splice(index, 1);
+      await prisma.user.update({
+        where: { email: email },
+        data: { bookedVisits: user.bookedVisits },
+      });
+      res.send("Booking canceled successfully");
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
