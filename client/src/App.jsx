d91a8effc2a20c1e70628dab6fa63217794e0eb0
RXGabriel <gabriel2005rf@gmail.com>
2024-06-04 09:50:35 -0300

    feat: add react-query with QueryClientProvider to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,24 @@ import Website from "./pages/Website";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Properties from "./pages/Properties/Properties";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Website />} />
-            <Route path="/properties" element={<Properties />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
