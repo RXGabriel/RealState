417b92bf6247657f74380a1aa09cd61736e2f49e
RXGabriel <gabriel2005rf@gmail.com>
2024-06-04 17:11:04 -0300

    feat: add facility details and icons to Property component

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -3,6 +3,9 @@ import { useLocation } from "react-router-dom";
 import { getProperty } from "../../utils/api";
 import { PuffLoader } from "react-spinners";
 import { AiFillHeart } from "react-icons/ai";
+import { FaShower } from "react-icons/fa";
+import { AiTwotoneCar } from "react-icons/ai";
+import { MdMeetingRoom } from "react-icons/md";
 import "./Property.css";
 
 const Property = () => {
@@ -52,6 +55,22 @@ const Property = () => {
               <span className="orangeText" style={{ fontSize: "1.5rem" }}>
                 $ {data?.price}
               </span>
+            </div>
+
+            {/* facilities */}
+            <div className="flexStart facilities">
+              <div className="flexStart facility">
+                <FaShower size={20} color="#1F3E72" />
+                <span>{data?.facilities.bathrooms} Bathrooms</span>
+              </div>
+              <div className="flexStart facility">
+                <AiTwotoneCar size={20} color="#1F3E72" />
+                <span>{data?.facilities.parkings} Parking</span>
+              </div>
+              <div className="flexStart facility">
+                <MdMeetingRoom size={20} color="#1F3E72" />
+                <span>{data?.facilities.bedrooms} Room/s</span>
+              </div>
             </div>
           </div>
         </div>
