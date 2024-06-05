8433952da44080b7faf70ad4ba0095a5388cefd7
RXGabriel <gabriel2005rf@gmail.com>
2024-06-05 16:07:44 -0300

    feat: Add Auth0 JWT authentication to user routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -7,14 +7,15 @@ import {
   getAllFavorites,
   toFav,
 } from "../controllers/userController.js";
+import jwtCheck from "../config/auth0Config.js";
 
 const router = express.Router();
 
-router.post("/register", createUser);
-router.post("/bookVisit/:id", bookVisit);
+router.post("/register", jwtCheck, createUser);
+router.post("/bookVisit/:id", jwtCheck, bookVisit);
 router.post("/allBookings", allBookings);
-router.post("/removeBooking/:id", cancelBooking);
-router.post("/toFav/:rid", toFav);
-router.post("/allFav/:rid", getAllFavorites);
+router.post("/removeBooking/:id", jwtCheck, cancelBooking);
+router.post("/toFav/:rid", jwtCheck, toFav);
+router.post("/allFav/:rid", jwtCheck, getAllFavorites);
 
 export { router as userRoute };
