b8ab9516d53b748e397fb439ab1e4974bfe949f0
RXGabriel <gabriel2005rf@gmail.com>
2024-06-05 12:07:29 -0300

    feat: integrate Auth0 authentication in Header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,10 +5,13 @@ import { getMenuStyles } from "../../utils/common";
 import useHeaderColor from "../../hooks/useHeaderColor";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import ProfileMenu from "../ProfileMenu/ProfileMenu";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const headerColor = useHeaderColor();
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
 
   return (
     <section className="h-wrapper" style={{ background: headerColor }}>
@@ -26,7 +29,13 @@ const Header = () => {
             <NavLink to="/properties">Properties</NavLink>
             <a href="mailto:examplesimple@gmail.com">Contact</a>
 
-            <button className="button">Login</button>
+            {!isAuthenticated ? (
+              <button className="button" onClick={loginWithRedirect}>
+                Login
+              </button>
+            ) : (
+              <ProfileMenu user={user} logout={logout} />
+            )}
           </div>
         </OutsideClickHandler>
 
